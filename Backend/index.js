08f0a81c94c986186f5e838bcb0daa11a977d375
Freetzen <federuizgei@gmail.com>
2024-02-25 21:20:20 -0300

    ewrg

--- Backend/index.js
+++ Backend/index.js
@@ -31,6 +31,7 @@ io.on('connection', (socket) => {
 
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(cors());
 app.use((req, res, next) => {
    res.header('Access-Control-Allow-Origin', '*');
    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
