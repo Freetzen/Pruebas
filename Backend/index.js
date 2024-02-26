3577f5d17f9717cc3f48e1dd9037f76f8b1ed27b
Freetzen <federuizgei@gmail.com>
2024-02-25 21:44:08 -0300

    sldgg

--- Backend/index.js
+++ Backend/index.js
@@ -33,7 +33,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 app.use((req, res, next) => {
-   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Origin', 'https://pruebas-orpin.vercel.app');
    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
    res.header('Access-Control-Allow-Headers', 'Content-Type'); 
    res.header('Access-Control-Allow-Credentials', true); // Habilitar credenciales
